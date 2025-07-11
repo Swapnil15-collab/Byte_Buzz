e7fa6cce26ba5e376d328f92d27ae75996da6fe4
swapnil panchadane <swapnilpanchadane237@gmail.com>
2025-07-11 19:19:13 +0530

    basics of cpp file for first content

---
+++ Basicscpp.php
@@ -0,0 +1,120 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Education website</title>
+
+    <!-- icons -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <link rel="stylesheet" href="style.css">
+
+
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
+
+
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+   <nav>
+    <div class="container nav__container">
+        <a href="index.php"><h4>ByteBuzz</h4></a>
+        <ul class="nav__menu">
+        
+
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="courses.php">COURSES</a></li>
+            <li><a href="prep2.php">INTERVIEW PREPARATION</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+           
+        
+
+        </ul>
+<button id="open-menu-btn"><i class="uil uil-bars" ></i> </button>
+<button id="close-menu-btn"><i class="uil uil-multiply" ></i></button>
+    
+    </div>
+   </nav> 
+<!-- delete all content for notes--> 
+
+
+<footer >
+    <div class="container footer__container">
+        <div class="footer__1">
+            <a href="index.php" class="footer__logo"><h4>ByteBuzz</h4></a>
+            <P>"Bringing education closer to you with carefully curated resources for every year of your computer science journey."</P>
+        </div>
+        <div class="footer__2">
+            <h4>Permalinks</h4>
+            <ul>
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="courses.html">COURSES</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+            </ul>
+        </div>
+      <div class="FOOTER__3">
+    <H4>Privacy</H4>    
+    <ul>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms and conditions</a></li>
+        <li><a href="#">Refund Policy</a></li>
+    </ul>
+    </div>
+    <div class="footer__4">
+        <h4>contact us</h4>
+        <div>
+            <p> +01 234 567 8910</p>
+        <p>swapnilpanchadane237@gmail.com</p>
+        </div>
+        
+    
+    <ul class="footer__socials">
+        <li>
+            <a href="#"><i class="uil uil-facebook-f"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="uil uil-instagram-alt"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="uil uil-twitter"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="uil uil-linkedin-alt"></i></a>
+        </li>
+    </ul>
+    
+<div class="footer__copyright">
+    <small>Copyright &copy : bytebuzz</small>
+</div>
+
+    </div>
+</div>
+</footer>
+
+
+    <!-- Add Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+    <script src="main.js"></script>
+
+
+
+
+
+
+
+
+</body>
+</html>
